25d14c0d62a66eb56aa678ed6891811d1790235f
RaetzkeMat <matthias.raetzke@zueblin.de>
2018-02-15 20:14:50 +0100

    1. steps

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Mauntzi');
+    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+    this.imgAttribution = ko.observable('artist');
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1 );
+    };
+    this.level = ko.computed(function(){
+        return this.clickCount() < 10 ? 'new born' : 'experienced cat';
+    },this);
+    
+}
+
+ko.applyBindings(new ViewModel());
+
+
+// 434164568_fea0ad4013_z.jpg
+// 1413379559_412a540d29_z.jpg
+// 4154543904_6e2428c421_z.jpg
+// 9648464288_2516b35537_z.jpg
